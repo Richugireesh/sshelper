ba96826b88a7bf11f3acfeb2b4f531b53f882ed0
Richugireesh <richugireesh@gmail.com>
2023-09-03 16:01:12 +0530

    Update Connection struct to handle optional fields
    - Modified the Connection struct to make the 'password' and 'key_path' fields optional.
    - This allows the JSON deserialization to succeed even if these fields are not present in the JSON data.

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,35 @@
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::Read;
+
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Connection {
+    name: String,
+    host: String,
+    port: u32,
+    username: String,
+    password: Option<String>,
+    key_path: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Data {
+    connections: Vec<Connection>,
+}
+
 fn main() {
     println!("Hello, world!");
-    let json = read_json();
-    print!("{}", json);
+    let data = read_and_parse_json("data/connections.json");
+    println!("{:?}", data);
 }
 
-// function to read from a json file
-fn read_json() -> String {
-    let mut file = File::open("data/connections.json").expect("file not found");
+// Function to read and parse from a JSON file
+fn read_and_parse_json(file_path: &str) -> Data {
+    let mut file = File::open(file_path).expect("File not found");
     let mut contents = String::new();
     file.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    contents
+        .expect("Something went wrong reading the file");
+    let parsed_data: Data = serde_json::from_str(&contents).unwrap();
+    parsed_data
 }
-
