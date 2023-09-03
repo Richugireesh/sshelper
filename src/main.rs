3da8d0bf494250c55b64449bdd82a4cc0654e2b4
Richugireesh <richugireesh@gmail.com>
2023-09-03 12:56:24 +0530

    read json file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs::File;
+use std::io::prelude::*;
 fn main() {
     println!("Hello, world!");
+    let json = read_json();
+    print!("{}", json);
 }
+
+// function to read from a json file
+fn read_json() -> String {
+    let mut file = File::open("data.json").expect("file not found");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    contents
+}
+
